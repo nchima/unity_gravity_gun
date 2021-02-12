3492563a1eea1dbcf33c6ffa78c5f18eff55c3c6
Unknown <atd335@nyu.edu>
2021-02-12 14:51:01 -0500

    Projectile gun, hit markers, and aiming arc

--- Assets/Scenes/Andrew/ShootTeleProjectile.cs
+++ Assets/Scenes/Andrew/ShootTeleProjectile.cs
@@ -11,35 +11,98 @@ public class ShootTeleProjectile : MonoBehaviour
     public GameObject gunModel;
     public GameObject projectile;
 
-    public float projectileSpeed;
+    public float projectileSpeed = 30;
+    public float projectileGravity = .8f;
 
     Transform hand;
+
+    int layerMask = 1 << 9;
 
     void Start()
     {
         GameObject g = Instantiate(gunModel, firstPersonCamera.transform);
         hand = g.transform;
+        if (!firstPersonCamera) { firstPersonCamera = GetComponent<Camera>(); }
     }
+
+    List<Vector3> trajPoints = new List<Vector3>();
+    //Vector3[] trajPoints = new Vector3[64];
+    Vector3 aimDir;
+
+    public Vector3 trajDest;
 
     void Update()
     {
-
-        for (int i = 0; i < 64; i++)
-        {
-
-        }
-
+        if (!firstPersonCamera) { print("No Camera Assigned!"); return; }
+        aimDir = ((firstPersonCamera.transform.position + (firstPersonCamera.transform.forward * 99999)) - hand.transform.position).normalized;
         if (Input.GetKeyDown(shootKey)) //runs when the assigend 'shoot' key is pressed
         {
             ShootProjectile(projectileSpeed);
         }
     }
 
+
+    private void LateUpdate()
+    {
+        Vector3 pos = hand.position;
+        Vector3 vel = aimDir * projectileSpeed;
+        Vector3 grav = new Vector3(0, -projectileGravity, 0) * projectileSpeed;
+        trajPoints.Clear();
+
+        Vector3 lastHit = Vector3.zero;
+        float lastDist = Mathf.Infinity;
+
+        int i = 0;
+
+        while (true)
+        {
+            trajPoints.Add(pos);
+            vel = vel + grav * Time.fixedDeltaTime;
+            pos = pos + vel * Time.fixedDeltaTime;
+            if (i > 0)
+            {
+                Debug.DrawLine(trajPoints[i], trajPoints[i - 1]);
+            }
+            if (Physics.Raycast(pos, vel.normalized, out RaycastHit trajHit))
+            {
+                if (lastDist < trajHit.distance)
+                {
+                    trajDest = lastHit;
+                    break;
+                }
+                lastHit = trajHit.point;
+                lastDist = trajHit.distance;
+            }
+            else
+            {
+                if (lastDist != Mathf.Infinity)
+                {
+                    trajDest = lastHit;
+                    break;
+                }
+            }
+            i++;
+            if (i > 128)
+            {
+                print("reaching limit...");
+                break;
+            }
+        }
+
+    }
+
     public void ShootProjectile(float spd)
     {
         GameObject p = Instantiate(projectile, hand.position, Quaternion.identity);
-        p.GetComponent<TeleProjectileScript>().moveDirection = ((firstPersonCamera.transform.position + (firstPersonCamera.transform.forward * 99999)) - p.transform.position).normalized;
+        p.GetComponent<TeleProjectileScript>().moveDirection = aimDir;
         //print(((firstPersonCamera.transform.position + (firstPersonCamera.transform.forward * 99999)) - p.transform.position).normalized);
         p.GetComponent<TeleProjectileScript>().speed = spd;
+        p.GetComponent<TeleProjectileScript>().projectileGravity = projectileGravity;
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawCube(trajDest,Vector3.one*.2f);
     }
 }
